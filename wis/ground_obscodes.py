a0c6a353fa0179eb8df7fad751f17930a7e9d0cc
matthewjohnpayne <matthewjohnpayne@gmail.com>
2021-01-27 08:56:17 -0500

    deleted old files

--- wis/ground_obscodes.py
+++
@@ -1,6 +0,0 @@
-# Define a handy dictionary containing all of the ground-based obscodes we are
-# currently set-up to handle in wis.py
-# --------------------------------------------------------------------------
-ground_obscode_dict = {}
-
-
